a112a3446544fc14a9a19e4298c399fe8d3f9cb9
Harsh Mule <hmuley22@gmail.com>
2020-07-20 12:01:09 +0530

    code intentations

--- Shop/urls.py
+++ Shop/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="ShopHome"),
-    path("about/",views.about,name="About"),
-    path("contact/",views.contact,name="ContactUs"),
-    path("tracker/",views.tracker,name="Tracker"),
-    path("search/",views.search,name="Search"),
-    path("products/<int:myid>",views.productview,name="Productview"),
-    path("checkout",views.checkout,name="Checkout")
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="About"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="Tracker"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productview, name="Productview"),
+    path("checkout", views.checkout, name="Checkout")
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,78 +1,92 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product,Contact,Orders,OrderUpdate
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
+
+
 def index(request):
-    #products = Product.objects.all()
-    #print(products)
-    #n = len(products)
-    #nSlides= n//4 + ceil((n/4)-(n//4))
-    allProds=[]
-    catprods= Product.objects.values('category','id')
-    cats={item['category'] for item in catprods}
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides= n//4 + ceil((n/4)-(n//4))
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
     for cat in cats:
-        prod=Product.objects.filter(category=cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides= n//4 + ceil((n/4)-(n//4))
-        allProds.append([prod, range(1,nSlides), nSlides])
-    #params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product':products}
-    #allProds=[[products, range(1,nSlides), nSlides],[products, range(1,nSlides), nSlides]]
-    params={'allProds':allProds}
-    return render(request,'Shop/index.html',params)
+        allProds.append([prod, range(1, nSlides), nSlides])
+    # params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product':products}
+    # allProds=[[products, range(1,nSlides), nSlides],[products, range(1,nSlides), nSlides]]
+    params = {'allProds' : allProds}
+    return render(request, 'Shop/index.html', params)
+
+
 def about(request):
-    return render(request,'Shop/about.html')
+    return render(request, 'Shop/about.html')
+
+
 def contact(request):
-    if request.method=="POST":
-        name=request.POST.get('name', '')
-        email=request.POST.get('email', '')
-        phone=request.POST.get('phone', '')
-        desc=request.POST.get('desc', '')
-        contact= Contact(name=name, email=email, phone=phone, desc=desc)
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-        done=True
-        return render(request,'Shop/contact.html',{'done':done})
-    return render(request,'Shop/contact.html')
+        done = True
+        return render(request, 'Shop/contact.html', {'done': done})
+    return render(request, 'Shop/contact.html')
+
+
 def tracker(request):
-    if request.method=="POST":
-        orderId=request.POST.get('orderId', '')
-        email=request.POST.get('email', '')
+    if request.method == "POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
         try:
-            order=Orders.objects.filter(order_id=orderId, email= email)
-            if len(order)>0:
-                update=OrderUpdate.objects.filter(order_id=orderId)
-                updates=[]
+            order = Orders.objects.filter(order_id=orderId, email= email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
                 for item in update:
-                    updates.append({'text':item.update_desc, 'time':item.timestamp})
-                    response=json.dumps([updates,order[0].items_json], default=str)
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
                 return HttpResponse(response)
             else:
                 return HttpResponse('{}')
         except Exception as e:
             return HttpResponse('{}')
-    return render(request,'Shop/tracker.html')
+    return render(request, 'Shop/tracker.html')
+
+
 def search(request):
-    return render(request,'Shop/search.html')
-def productview(request,myid):
-    #Fetching the product using ID
+    return render(request, 'Shop/search.html')
+
+
+def productview(request, myid):
+    # Fetching the product using ID
     product = Product.objects.filter(id=myid)
-    return render(request,'Shop/productview.html',{'product':product[0]})
+    return render(request, 'Shop/productview.html', {'product': product[0]})
+
+
 def checkout(request):
-    if request.method=="POST":
-        items_json=request.POST.get('itemsJson','')
-        name=request.POST.get('name', '')
-        email=request.POST.get('email', '')
-        address=request.POST.get('address1', '') +" "+request.POST.get('address2', '')
-        city=request.POST.get('city', '')
-        state=request.POST.get('state', '')
-        zip_code=request.POST.get('zip_code', '')
-        phone=request.POST.get('phone', '')
-        order= Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, zip_code=zip_code , phone=phone )
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, zip_code=zip_code , phone=phone )
         order.save()
-        update=OrderUpdate(order_id=order.order_id, update_desc="The Order has been placed")
+        update = OrderUpdate(order_id=order.order_id, update_desc="The Order has been placed")
         update.save()
         thank = True
         id = order.order_id
-        return render(request,'Shop/checkout.html',{'thank':thank, 'id':id})
-    return render(request,'Shop/checkout.html')
+        return render(request, 'Shop/checkout.html', {'thank': thank, 'id': id})
+    return render(request, 'Shop/checkout.html')
 
